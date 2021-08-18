032935918d6ede23cc58f7115154336053ee83ee
farrelramadhika <farrelramadhika@gmail.com>
2021-08-18 20:47:02 +0700

    + layout

---
+++ app/src/main/java/com/example/recentmovies/Api_Url.kt
@@ -0,0 +1,7 @@
+package com.example.recentmovies
+
+object Api_Url {
+    val API_URL = "https://api.themoviedb.org/3/"
+    val Token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OThjNDg1MDhkYWRhYWQ4NmNlNDM1ZjllMzJkN2RjMiIsInN1YiI6IjYxMWJhN2FjMmRjNDRlMDAyYzQzNzNiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1K_IxsBvboUdbMOt8VCGaxc7haePzQIdWa592goKSKs"
+    val API_KEY = "698c48508dadaad86ce435f9e32d7dc2"
+}
--- app/src/main/java/com/example/recentmovies/HomeMoviesFragment.kt
+++ app/src/main/java/com/example/recentmovies/HomeMoviesFragment.kt
@@ -5,28 +5,60 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SimpleItemAnimator
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.BasicNetwork
+import com.android.volley.toolbox.DiskBasedCache
+import com.android.volley.toolbox.HurlStack
+import com.example.recentmovies.Api_Url.API_URL
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeMoviesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeMoviesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    var adapterView = GroupAdapter<ViewHolder>()
+    var recviewHome : RecyclerView? = null
+    var recviewPopular : RecyclerView? = null
+    var progressHome : ProgressBar? = null
+    var URL = API_URL
+    val Token = Api_Url.Token
+    val API_KEY = Api_Url.API_KEY
+
+    private var requestQueue : RequestQueue? = null
+    private var idgenre : String? = null
+    private var namegenre : String? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_movies, container, false)
+        val view : View = inflater.inflate(R.layout.fragment_home_movies, container, false)
+
+        val cache = DiskBasedCache(requireActivity().cacheDir, 1024 * 1024) // 1MB cap
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        val network = BasicNetwork(HurlStack())
+
+        recviewHome = view.findViewById(R.id.rvgenre)
+        recviewPopular = view.findViewById(R.id.rvpopular)
+        progressHome = view.findViewById(R.id.progressHome)
+
+        val layoutManager = GridLayoutManager(getActivity(),1)
+        (recviewHome!!.getItemAnimator() as SimpleItemAnimator).setSupportsChangeAnimations(false)
+        recviewHome!!.setLayoutManager(layoutManager)
+
+
+        return view
     }
 
 
